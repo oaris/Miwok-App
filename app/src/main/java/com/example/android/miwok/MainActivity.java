45ca4275a67c7abdd6b22909e16909f819b14340
Brandon Hancock <behancoc@gmail.com>
2017-11-12 11:54:21 -0800

    Corrects issue w/ my code from end of Lesson 1 that was causing MainActivity to crash

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -21,13 +21,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
 
         TextView numbers = (TextView) findViewById(R.id.numbers);
         numbers.setOnClickListener(new View.OnClickListener() {
@@ -64,8 +64,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(colorsIntent);
             }
         });
-
-        // Set the content of the activity to use the activity_main.xml layout file
-        setContentView(R.layout.activity_main);
     }
 }
